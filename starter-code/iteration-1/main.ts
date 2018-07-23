e955e51e0372df93eec64f991cb8b3744e6c62cc
Maxim Chadliev <chad0441@gmail.com>
2018-07-23 10:35:31 -0400

    iteraiton 1 done

--- starter-code/iteration-1/main.ts
+++ starter-code/iteration-1/main.ts
@@ -1,7 +1,17 @@
 // Iteration 1
 // 1. Create an array of strings
 
+
+let myArray: Array<string> = []; // Array of strings
+
+
 // 2. Create a function to add a task in to the array, call it `addTask`, the function has to accept a string and don't have to return anything.
+
+
+function addTask(task: string){
+   myArray.push(task);
+   console.log(myArray);
+}
 
 // 3. Create a function to list all tasks, it must show in the console de task.
 
@@ -13,6 +23,6 @@ console.log("Number of items:", addTask('Eat pizza 🍕 yummy!!!'));
 console.log("Number of items:", addTask('Finish this iteration 1!! 🤓'));
 console.log("Number of items:", addTask('Finish this iteration 2!! 🤓'));
 console.log("Number of items:", addTask('Finish this iteration 3!! 🤓'));
-listAllTasks();
-console.log("Number of items:", deleteTask('Finish this iteration 1!! 🤓'));
-listAllTasks();
+// listAllTasks();
+// console.log("Number of items:", deleteTask('Finish this iteration 1!! 🤓'));
+// listAllTasks();
